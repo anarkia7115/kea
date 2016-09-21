926ea117d162bb3fc4e5febc16a366bad199fb65
shawn <shawn24601@gmail.com>
2016-09-21 14:19:52 +0800

    new

---
+++ src/main/java/shawn/gcbi/com/kea/App.java
@@ -0,0 +1,13 @@
+package shawn.gcbi.com.kea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
